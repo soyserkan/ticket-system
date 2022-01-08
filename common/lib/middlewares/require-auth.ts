38ab40f07e8c5b54c4464fdc3536189377473964
Serkan Soy <serkan.soy@pixasoftware.com>
2022-01-09 00:37:01 +0300

    npm common module updates

--- common/lib/middlewares/require-auth.ts
+++ common/lib/middlewares/require-auth.ts
@@ -2,12 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { UnauthorizedError } from "..";
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    try {
-        if (!req.currentUser) {
-            throw new UnauthorizedError();
-        }
-        next();
-    } catch (error) {
-        next();
+    if (!req.currentUser) {
+        throw new UnauthorizedError();
     }
+    next();
 };
