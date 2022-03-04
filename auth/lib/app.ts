9b4f9643828f378325c1d6f335d61ae71f48d639
soyserkan <serkansoy94@gmail.com>
2022-03-05 00:13:48 +0300

    test

--- auth/lib/app.ts
+++ auth/lib/app.ts
@@ -34,7 +34,7 @@ export class App {
         this.app.use(bodyParser.json());
         this.app.use(cors.default());
         this.app.use(helmet());
-        this.app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' }));
+        this.app.use(cookieSession({ signed: false, secure: false }));
     }
     async listen(server: Server) {
         try {
