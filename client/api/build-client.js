9820dec5c19ee6f3317cffdd80e5dc97c0676243
soyserkan <serkansoy94@gmail.com>
2022-03-05 11:32:00 +0300

    test

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ import axios from "axios"
 export default function ({ req }) {
     if (typeof window === "undefined") {
         return axios.create({
-            //baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-            baseURL: 'http://157.245.24.165/',
+            // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.ticketsystem-serkan.xyz/',
             headers: req.headers
         });
     } else {
