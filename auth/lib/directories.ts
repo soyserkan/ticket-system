c48b8e820f39248c0b41777eafae576bfdec97cd
“soyserkan” <101094351+serkanpixa@users.noreply.github.com>
2022-03-16 00:16:28 +0300

    deneme

--- auth/lib/directories.ts
+++ auth/lib/directories.ts
@@ -5,9 +5,10 @@ export class Directories {
     static scripts: string = `${Directories.main}/../scripts`;
     static public: string = `${Directories.main}/../public`;
     static images: string = `${Directories.public}/images`;
+    static log: string = `${Directories.public}/log`;
 
     static async createDirectories() {
-        var directories = [Directories.scripts, Directories.public, Directories.images];
+        var directories = [Directories.scripts, Directories.public, Directories.images, Directories.log];
         for (var dir of directories) {
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
